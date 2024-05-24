c396c32808baa843b87f4890465abd3536b18cf0
Viswath Senthilraj <viswathleon@gmail.com>
2024-05-24 18:36:19 +0530

    chore: intial setup completed

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,13 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Budget Tracker</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text style={{}}>Budget Tracker</Text>
+      <StatusBar style="auto" />
+      <Link href="Profile">Profile</Link>
     </View>
   );
 }
